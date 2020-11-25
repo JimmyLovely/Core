8a178cc2cb647b073f4efc6a293881087888756d
Li <Zhimin.Li@elekta.com>
2020-11-25 17:39:18 +0800

    .Net Core: Lambda, `lambda expression` and lambda statement, Action and Func (#13)

---
+++ Controllers/LambdaController.cs
@@ -0,0 +1,60 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+using System.Linq;
+using System.Linq.Expressions;
+using System.Collections.Generic;
+
+namespace NetCore.Controllers
+{
+    [Route("[controller]")]
+    public class LambdaController : ControllerBase
+    {
+        [HttpGet]
+        [Route("demo")]
+        public IActionResult Demo()
+        {
+            Func<int, int> square = x => x * x;
+            Console.WriteLine(square(5));
+            Console.WriteLine(square);
+
+            Expression<Func<int, int>> squareExpression = x => x * x;
+            Console.WriteLine(square);
+            Console.WriteLine(square(5));
+
+            int[] tmpArray = { 1, 2, 3, 4, 5 };
+            IEnumerable<int> filterArray = tmpArray.Select(x => x * x);
+            foreach (int item in filterArray)
+            {
+                Console.WriteLine(item);
+            }
+
+            var filterArray2 = Enumerable.Select<int, int>(tmpArray, x => x * x);
+            foreach (int item in filterArray2)
+            {
+                Console.WriteLine(item);
+            }
+            var filterArray3 = Enumerable.Select<int, int>(tmpArray, square);
+            foreach (int item in filterArray3)
+            {
+                Console.WriteLine(item);
+            }
+
+            Action<int> line = (x) => Console.WriteLine(x);
+
+            Func<string> Func1String = () => string.Empty;
+
+            Func<int, string> Func2String = (x) => (x * x).ToString();
+
+            Func<int, string> Func2StringWithout = x => (x * x).ToString();
+
+            Func<int, int, string> Func3String = (x, y) => (x * y).ToString();
+
+            Func<int, int, int> Func3StringWithoutParameter = (_, x) => _ * 3;
+
+            // Func<int, int, int, int> Func3StringWithoutParameter2 = (_, _, x) => x * 3;
+
+            return Ok();
+        }
+    }
+}
