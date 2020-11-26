d9c2a84db0417afdafc382160a0e5e8a22f4cd67
Li <Zhimin.Li@elekta.com>
2020-11-26 15:12:58 +0800

    .Net Core: Lambda, `lambda expression` abd lambda rule(#13)

--- Controllers/LambdaController.cs
+++ Controllers/LambdaController.cs
@@ -54,6 +54,30 @@ namespace NetCore.Controllers
 
             // Func<int, int, int, int> Func3StringWithoutParameter2 = (_, _, x) => x * 3;
 
+            // tuple
+            Func<(int a, int b, int c), (int, int, int)> Func3IntWithTuple = abc => (abc.a * 2, abc.b * 2, abc.Item3 * 2);
+
+            (int, int, int) tuple3Int = (1, 2, 3);
+            (int, int, int) tuple3IntResult = Func3IntWithTuple(tuple3Int);
+            Console.WriteLine(tuple3Int);
+            Console.WriteLine(tuple3IntResult);
+
+            int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+
+            // Count
+            Func<int, bool> numberWithCountFunc = n => n > 6;
+
+            int numberWithCount1 = numbers.Count(n => n > 6);
+            int numberWithCount2 = numbers.Count(numberWithCountFunc);
+            int numberWithCount3 = Enumerable.Count<int>(numbers, numberWithCountFunc);
+
+            // TakeWhile
+            Func<int, int, bool> numberWithTakeWhileFunc = (n, index) => n >= index;
+
+            IEnumerable<int> numberWithTakeWhile1 = numbers.TakeWhile((n, index) => n >= index);
+            IEnumerable<int> numberWithTakeWhile2 = numbers.TakeWhile(numberWithTakeWhileFunc);
+            IEnumerable<int> numberWithTakeWhile3 = Enumerable.TakeWhile<int>(numbers, numberWithTakeWhileFunc);
+
             return Ok();
         }
     }
